ac2d23716e1b3cfe604e4230779f4e7a8c2a19fd
Meelinank <99127365+Meelinank@users.noreply.github.com>
2023-10-09 11:53:52 +0200

    This is a test to: Add Boost ASIO client to send a greeting message to a server.
    - Initialize Boost ASIO's IO context.
    - Resolve the IP and port of the target server.
    - Establish a TCP connection using the resolved endpoint.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <boost/asio.hpp>
 
-int main () {
-    std::cout << "Hello World!" << std::endl;
+using boost::asio::ip::tcp;
+
+int main() {
+    boost::asio::io_service io_service;
+
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query("RASPBERRY_PI_IP_ADDRESS", "12345");
+    tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoint_iterator);
+
+    std::string message = "Hello from C++!";
+    boost::asio::write(socket, boost::asio::buffer(message));
+
     return 0;
 }
