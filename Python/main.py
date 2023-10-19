0d66bf5ef2cb011f2bd16498dc3bde2248d08df5
Thomas Hellstrøm Olsen <74154916+Hellsparks@users.noreply.github.com>
2023-10-19 15:11:16 +0200

    testing new gpt code

--- Python/main.py
+++ Python/main.py
@@ -1,25 +1,12 @@
 import socket
 import picamera
 import time
+import io
+
 # Set up camera
 camera = picamera.PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 24
-
-"""
-HOST = '0.0.0.0'  # Listen on all interfaces
-PORT = 12345
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print('Server listening...')
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        data = conn.recv(1024)
-        print('Received:', data.decode())
-"""
 
 # Set up UDP socket
 UDP_IP = "127.0.0.1"
@@ -27,10 +14,23 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Start recording
-camera.start_recording('udp://{}:{}'.format(UDP_IP, UDP_PORT), format='h264')
+stream = io.BytesIO()
+camera.start_recording(stream, format='h264')
+try:
+    while True:
+        # Wait for the buffer to fill up
+        time.sleep(1)
 
-# Stop recording
-#camera.stop_recording()
+        # Send the buffer over UDP
+        sock.sendto(stream.getvalue(), (UDP_IP, UDP_PORT))
 
-# Close socket
-#sock.close()
+        # Reset the buffer for the next frame
+        stream.seek(0)
+        stream.truncate()
+
+finally:
+    # Stop recording
+    camera.stop_recording()
+
+    # Close socket
+    sock.close()
