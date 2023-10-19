1b16bedd5d4a24c8d3c1c4d5baa131d22cd7c08f
Thomas Hellstrøm Olsen <74154916+Hellsparks@users.noreply.github.com>
2023-10-19 14:17:23 +0200

    testing webcam stream

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,10 @@
 import socket
+import picamera
+import time
+# Set up camera
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 24
 
 HOST = '0.0.0.0'  # Listen on all interfaces
 PORT = 12345
@@ -12,3 +18,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Connected by', addr)
         data = conn.recv(1024)
         print('Received:', data.decode())
+
+
+
+# Set up UDP socket
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Start recording
+camera.start_recording('udp://{}:{}'.format(UDP_IP, UDP_PORT), format='h264')
+
+# Stop recording
+#camera.stop_recording()
+
+# Close socket
+#sock.close()
--- Python/webcamTest.py
+++ Python/webcamTest.py
@@ -1,101 +1,34 @@
 import cv2
-import asyncio
-import websockets
+import numpy as np
+import socket
 
-async def send_video():
-    # Open the default camera
-    cap = cv2.VideoCapture(0)
+# Set up UDP socket
+UDP_IP = "192.168.199.175"
+UDP_PORT = 12345
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
 
-    # Set the video size to 640x480
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# Set up OpenCV window
+cv2.namedWindow("Video", cv2.WINDOW_NORMAL)
 
-    # Create a UDP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Receive and display video stream
+while True:
+    # Receive frame from UDP socket
+    data, addr = sock.recvfrom(65507)
 
-    # Connect to the server
-    server_address = ("192.168.199.175", 12345)
+    # Convert frame to numpy array
+    frame = np.frombuffer(data, dtype=np.uint8)
 
-    # Start the video stream
-    while True:
-        # Capture a frame from the camera
-        ret, frame = cap.read()
+    # Decode frame
+    frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
 
-        # Convert the frame to a string
-        frame_str = cv2.imencode('.jpg', frame)[1].tostring()
+    # Display frame
+    cv2.imshow("Video", frame)
 
-        # Send the frame over UDP
-        sock.sendto(frame_str, server_address)
+    # Exit if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-        # Wait for a short time
-        await asyncio.sleep(0.01)
-
-    # Release the camera and close the socket
-    cap.release()
-    sock.close()
-
-async def main():
-    async with websockets.connect('ws://localhost:8765') as websocket:
-        await websocket.send('start')
-        await send_video()
-
-asyncio.run(main())
-"""
-// FILEPATH: /path/to/receive_video.cpp
-#include <opencv2/opencv.hpp>
-#include <iostream>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-
-#define PORT 12345
-
-int main() {
-    // Create a UDP socket
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-        std::cerr << "Failed to create socket" << std::endl;
-        return 1;
-    }
-
-    // Bind the socket to a local address
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(PORT);
-    if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
-        std::cerr << "Failed to bind socket" << std::endl;
-        return 1;
-    }
-
-    // Create a window to display the video
-    cv::namedWindow("Video", cv::WINDOW_NORMAL);
-
-    // Receive and display the video stream
-    while (true) {
-        // Receive a frame from the socket
-        char buffer[640*480*3];
-        struct sockaddr_in client_addr;
-        socklen_t client_len = sizeof(client_addr);
-        ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_addr, &client_len);
-        if (n < 0) {
-            std::cerr << "Failed to receive frame" << std::endl;
-            break;
-        }
-
-        // Convert the frame to an OpenCV image
-        cv::Mat frame = cv::imdecode(cv::Mat(1, n, CV_8UC1, buffer), cv::IMREAD_COLOR);
-
-        // Display the frame
-        cv::imshow("Video", frame);
-        cv::waitKey(1);
-    }
-
-    // Close the socket and destroy the window
-    close(sockfd);
-    cv::destroyAllWindows();
-
-    return 0;
-}
-"""
+# Close socket and OpenCV window
+sock.close()
+cv2.destroyAllWindows()
