ec5a5db33ef59c610e3df345378ef7458433b96f
Meelinank <99127365+Meelinank@users.noreply.github.com>
2023-11-06 12:02:02 +0100

    Implemented code to stream video from server.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,47 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <opencv2/opencv.hpp>
 
 using boost::asio::ip::tcp;
 
 int main() {
+
+    // Connect to the server
     boost::asio::io_service io_service;
+    tcp::socket socket(io_service);
+    socket.connect(tcp::endpoint(boost::asio::ip::address::from_string("10.25.45.112"), 8000));
 
     try {
-        tcp::resolver resolver(io_service);
-        tcp::resolver::query query("192.168.199.212", "12345");
-        tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+        while (true) {
+            boost::asio::streambuf b;
+            boost::asio::read(socket, b, boost::asio::transfer_exactly(4));  // Read the size of the image
+            std::istream is(&b);
+            uint32_t image_len;
+            is.read(reinterpret_cast<char *>(&image_len), 4);
 
-        tcp::socket socket(io_service);
-        boost::asio::connect(socket, endpoint_iterator);
+            // Allocate a buffer and read the image data
+            std::vector<uchar> buffer(image_len);
+            boost::asio::read(socket, boost::asio::buffer(buffer.data(), image_len));
 
-        std::string message = "Hello from C++!";
-        boost::asio::write(socket, boost::asio::buffer(message));
-    } catch (std::exception& e) {
+            // Decode the image
+            cv::Mat frame = imdecode(cv::Mat(buffer), cv::IMREAD_COLOR);
+            if (frame.empty()) {
+                std::cerr << "Received an empty frame!" << std::endl;
+                break;
+            }
+
+            // Display the frame
+            imshow("Video", frame);
+            if (cv::waitKey(1) == 'q') {
+                break;
+            }
+        }
+    } catch (std::exception &e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
+
+    // Close the connection
+    socket.close();
 
     return 0;
 }
