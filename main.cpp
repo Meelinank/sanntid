98901b7109108af51efb6fc961247204f51da42a
Marnerl <marnerl@stud.ntnu.no>
2023-11-25 14:27:35 +0100

    UI first implementation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
+#define CVUI_IMPLEMENTATION
 #include <iostream>
 #include <string>
 #include <thread>
 #include <vector>
 #include <boost/asio.hpp>
-#include <SDL2/SDL.h>
+//#include <SDL2/SDL.h>
 #include <opencv2/opencv.hpp>
+#include "UI/cvui.h"
+
+#define WINDOW_NAME "Sphero Control"
 
 using boost::asio::ip::tcp;
 
@@ -53,63 +57,125 @@ void videoStreamThread() {
     }
 }
 
+
 int main() {
-    try {
-        boost::asio::io_context io_context;
-        tcp::socket socket(io_context);
-        global_socket = &socket;
+    cvui::init(WINDOW_NAME);
+    cv::Mat frame = cv::Mat(400, 600, CV_8UC3);
 
-        tcp::resolver resolver(io_context);
-        auto endpoints = resolver.resolve("192.168.1.166", "8050");
-        boost::asio::connect(socket, endpoints);
+    while (true) {
+        // Clear the frame
+        frame.setTo(cv::Scalar(49, 52, 49));
 
-        std::thread videoThread(videoStreamThread);
+        // Check for mouse events
+        cv::Point mouse = cvui::mouse();
 
-        if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-            std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
-            return 1;
+        // Buttons for manual steering
+        if (cvui::button(frame, 50, 50, "Forward")) {
+            if (cvui::mouse(cvui::CLICK)) {
+                // Handle the "FORWARD" button click, implement your action here
+                std::cout << "Sending FORWARD command to the robot." << std::endl;
+            }
         }
-
-        std::cout << "Connected to the server. Press arrow keys to control the robot. Press 'q' to quit." << std::endl;
-
-        bool quit = false;
-        SDL_Event e;
-
-        while (!quit) {
-            while (SDL_PollEvent(&e) != 0) {
-                if (e.type == SDL_QUIT) {
-                    quit = true;
-                } else if (e.type == SDL_KEYDOWN) {
-                    switch (e.key.keysym.sym) {
-                        case SDLK_UP: send_command(socket, "FORWARD"); break;
-                        case SDLK_DOWN: send_command(socket, "BACKWARD"); break;
-                        case SDLK_LEFT: send_command(socket, "LEFT"); break;
-                        case SDLK_RIGHT: send_command(socket, "RIGHT"); break;
-                        case SDLK_s: send_command(socket, "STOP"); break;
-                        case SDLK_q: quit = true; break;
-                    }
-                }
+        if (cvui::button(frame, 50, 150, "Backward")) {
+            if (cvui::mouse(cvui::CLICK)) {
+                // Handle the "BACKWARD" button click, implement your action here
+                std::cout << "Sending BACKWARD command to the robot." << std::endl;
             }
-
-            {
-                std::lock_guard<std::mutex> lock(image_mutex);
-                if (new_frame_available) {
-                    cv::imshow("Video", global_frame);
-                    new_frame_available = false;
-                }
+        }
+        if (cvui::button(frame, 200, 100, "Left")) {
+            if (cvui::mouse(cvui::CLICK)) {
+                // Handle the "LEFT" button click, implement your action here
+                std::cout << "Sending LEFT command to the robot." << std::endl;
             }
-
-            if (cv::waitKey(1) == 'q') {
-                break;
+        }
+        if (cvui::button(frame, 350, 100, "Right")) {
+            if (cvui::mouse(cvui::CLICK)) {
+                // Handle the "RIGHT" button click, implement your action here
+                std::cout << "Sending RIGHT command to the robot." << std::endl;
             }
         }
 
-        videoThread.join();
-        SDL_Quit();
-    } catch (const std::exception& e) {
-        std::cerr << "Main Exception: " << e.what() << std::endl;
-        SDL_Quit();
+        // Display the UI frame
+        cvui::update();
+        cv::imshow(WINDOW_NAME, frame);
+
+        // Check for keyboard interactions (e.g., exit the loop)
+        char presskey = cv::waitKey(20);
+        if (presskey == 'q' || presskey == 27) {
+            std::cout << "Quit." << std::endl;
+            break;
+        }
     }
 
-    return 0;
-}
+/*
+        try {
+            boost::asio::io_context io_context;
+            tcp::socket socket(io_context);
+            global_socket = &socket;
+
+            tcp::resolver resolver(io_context);
+            auto endpoints = resolver.resolve("10.25.45.112", "8050");
+            boost::asio::connect(socket, endpoints);
+            std::thread videoThread(videoStreamThread);
+
+            if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+                std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+                return 1;
+            }
+
+            std::cout << "Connected to the server. Press arrow keys to control the robot. Press 'q' to quit."
+                      << std::endl;
+
+            bool quit = false;
+            SDL_Event e;
+
+            while (!quit) {
+                while (SDL_PollEvent(&e) != 0) {
+                    if (e.type == SDL_QUIT) {
+                        quit = true;
+                    } else if (e.type == SDL_KEYDOWN) {
+                        switch (e.key.keysym.sym) {
+                            case SDLK_UP:
+                                send_command(socket, "FORWARD");
+                                break;
+                            case SDLK_DOWN:
+                                send_command(socket, "BACKWARD");
+                                break;
+                            case SDLK_LEFT:
+                                send_command(socket, "LEFT");
+                                break;
+                            case SDLK_RIGHT:
+                                send_command(socket, "RIGHT");
+                                break;
+                            case SDLK_s:
+                                send_command(socket, "STOP");
+                                break;
+                            case SDLK_q:
+                                quit = true;
+                                break;
+                        }
+                    }
+                }
+
+                {
+                    std::lock_guard<std::mutex> lock(image_mutex);
+                    if (new_frame_available) {
+                        cv::imshow("Video", global_frame);
+                        new_frame_available = false;
+                    }
+                }
+
+                if (cv::waitKey(1) == 'q') {
+                    break;
+                }
+            }
+
+            videoThread.join();
+            SDL_Quit();
+        } catch (const std::exception &e) {
+            std::cerr << "Main Exception: " << e.what() << std::endl;
+            SDL_Quit();
+        }
+*/
+        return 0;
+    }
