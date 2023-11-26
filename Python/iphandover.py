2e5c115f0b4a188ca769eaf8f9758125a354a92b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-26 18:01:08 +0100

    fixed bugs

--- Python/iphandover.py
+++ Python/iphandover.py
@@ -1,21 +1,28 @@
-https://discord.com/api/webhooks/1178373329044914196/YNtaZku0tmbM9Iy0MSNHoqgoY6S2XJ4qasBynLkYw21TQm4VY3TR5P7fqTFKcMkhCopy
-
 import socket
 import requests
-import urllib.request
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
-webhookURL = '<webhook url>'
+
+
+webhookURL = 'https://discord.com/api/webhooks/1178373329044914196/YNtaZku0tmbM9Iy0MSNHoqgoY6S2XJ4qasBynLkYw21TQm4VY3TR5P7fqTFKcMkhCopy'
 hostname = socket.gethostname()
-external_ipv6 = urllib.request.urlopen('https://v6.ident.me').read().decode('utf8')
-external_ipv4 = socket.gethostbyname(hostname)
-#urllib.request.urlopen('https://v4.ident.me').read().decode('utf8')
-
+localIP = get_ip()#socket.gethostbyname(hostname)
 
 data = {
-    'username': 'https://discord.com/api/webhooks/1178373329044914196/YNtaZku0tmbM9Iy0MSNHoqgoY6S2XJ4qasBynLkYw21TQm4VY3TR5P7fqTFKcMkhCopy',
+    'username': 'Server IP Webhook',
     'embeds': [{
         'title': 'Server started!',
-        'description': hostname + '\n' + 'IPV6' + ': ' + '`' + external_ipv6 + '`'  + '\n\n' + 'IPV4 ' + ':'  + '`' + external_ipv4 + '`',
+        'description': hostname + ': ' + localIP,
     }]
 }
 
