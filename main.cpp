d588a3448814349b8d22fc112cde0a643acc6630
Marnerl <marnerl@stud.ntnu.no>
2023-12-12 17:01:34 +0100

    Expanded UI functions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "RobotController.hpp"
 #include <boost/asio.hpp>
 
+
 #define WINDOW_NAME "Robot Control" // Name of the window
 
 int main() {
@@ -15,6 +16,7 @@ int main() {
 
     float speed = 0;
     bool manualMode = true;
+
     frameReceiver.startReceiving();
     robotController.start();
     cvui::init(WINDOW_NAME);
@@ -32,11 +34,22 @@ int main() {
             cv::imshow("Video Stream", videoWindow);
         }
 
-        cvui::trackbar(frame, 50, 300, 180, &speed, (float)0.5, (float)1.125);
+        cvui::trackbar(frame, 50, 340, 360, &speed, (float)0.5, (float)1.125);
 
         int key = cv::waitKey(20); // Declare 'key' here so it's available throughout the loop
 
+        // Window displaying driving mode selection
+        cvui::window(frame, 50, 200, 180, 120, "Select driving mode");
+        if (cvui::button(frame, 80, 240, "Manual")) {
+            manualMode = true;
+        }
+        if (cvui::button(frame, 80, 280, "Automatic")) {
+            manualMode = false;
+        }
+
         if (manualMode) {
+            // text displaying when in manual mode
+            cvui::text(frame, 250, 50, "Manual Control Active:");
             nlohmann::json j;
             if (key == 119) {  // 'w' key for Forward
                 j["command"] = "F";
@@ -53,19 +66,32 @@ int main() {
             } else if (key == 32) {  // Space bar for Stop
                 j["command"] = "S";
             }
+
             if (!j.empty()) {
                 std::string jsonString = j.dump();
                 std::cout << jsonString << std::endl;
                 commandSender.sendCommand(jsonString);
             }
+
         } else {
             if (!videoFrame.empty()) {
                 robotController.processFrame(videoFrame);
                 robotController.setSpeed(speed);
             }
+            // text displaying when in automatic mode
+            cvui::text(frame, 250, 50, "Automatic Control Active:");
         }
 
-        cvui::checkbox(frame, 50, 250, "Autonomous Mode", &manualMode);
+        //cvui::checkbox(frame, 50, 250, "Autonomous Mode", &manualMode);
+
+        // Display manual control buttons
+        cvui::text(frame, 50, 50, "Manual Control:");
+        cvui::text(frame, 50, 70, "W - Forward");
+        cvui::text(frame, 50, 90, "A - Left");
+        cvui::text(frame, 50, 110, "S - Backward");
+        cvui::text(frame, 50, 130, "D - Right");
+        cvui::text(frame, 50, 150, "Space - Stop");
+        cvui::text(frame, 250, 200, "ESC - Quit");
 
         cvui::update();
         cv::imshow(WINDOW_NAME, frame);
