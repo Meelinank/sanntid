9c27ceb2827043f6c56730ee4b9690e2b22b9603
Thomas Hellstrøm Olsen <74154916+Hellsparks@users.noreply.github.com>
2023-12-14 16:18:31 +0100

    while loops

--- Python/sensortester.py
+++ Python/sensortester.py
@@ -2,10 +2,7 @@ import os
 import sys
 import time
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
-
 from sphero_sdk import SpheroRvrObserver
-
-
 rvr = SpheroRvrObserver()
 
 
@@ -16,22 +13,18 @@ def battery_voltage_state_change_handler(battery_voltage_state):
 def main():
     """ This program demonstrates how to enable battery state change notifications.
     """
-
-    try:
-        rvr.wake()
+    rvr.wake()
 
         # Give RVR time to wake up
-        time.sleep(2)
-
-        rvr.on_battery_voltage_state_change_notify(handler=battery_voltage_state_change_handler)
-        rvr.enable_battery_voltage_state_change_notify(is_enabled=True)
-
-    except KeyboardInterrupt:
-        print('\nProgram terminated with keyboard interrupt.')
-
-    finally:
-        rvr.close()
-
+    time.sleep(1)
+    rvr.enable_battery_voltage_state_change_notify(is_enabled=True)
+    while True:
+        try:
+            rvr.on_battery_voltage_state_change_notify(handler=battery_voltage_state_change_handler)
+        except KeyboardInterrupt:
+            print('\nProgram terminated with keyboard interrupt.')
+        finally:
+            rvr.close()
 
 if __name__ == '__main__':
-    main()
+        main()
