404a829149c1f6a2dfa75a2823a84313d9e05df1
Marnerl <marnerl@stud.ntnu.no>
2023-12-15 13:26:05 +0100

    Updated info about Tests
    Comments on what test works & does not so far

--- Tests/tester.cpp
+++ Tests/tester.cpp
@@ -4,23 +4,23 @@
 #include "SensorDataReceiver.hpp"
 
 
-TEST_CASE("Testing CommandSender", "[Testing]") {
+TEST_CASE("Testing CommandSender", "[Testing]") { // This test works
     boost::asio::io_service io_service;
     CommandSender commandSender(io_service, "127.0.0.1", "5001");
 }
 
-TEST_CASE("Testing RobotController", "[Testing]") {
+TEST_CASE("Testing RobotController", "[Testing]") { // This test does not work
     boost::asio::io_service io_service;
     CommandSender commandSender(io_service, "127.0.0.1", "5001");
     RobotController robotController("127.0.0.1", "5000", "127.0.0.1", "5001", io_service, commandSender);
 }
 
-TEST_CASE("Testing FrameReceiver", "[Testing]") {
+TEST_CASE("Testing FrameReceiver", "[Testing]") { // This test does not work
     boost::asio::io_service io_service;
     FrameReceiver frameReceiver(io_service, "127.0.0.1", "5000");
 }
 
-TEST_CASE("Testing SensorDataReceiver", "[Testing]") {
+TEST_CASE("Testing SensorDataReceiver", "[Testing]") { // This test works
     boost::asio::io_service io_service;
     SensorDataReceiver sensorDataReceiver(io_service, "127.0.0.1", "5002");
 }
