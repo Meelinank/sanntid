002ee40f3895b7cdc40b46858bbfdbc2f4bb051a
Marnerl <marnerl@stud.ntnu.no>
2023-12-15 16:59:21 +0100

    Updated README and some comments

--- Tests/tester.cpp
+++ Tests/tester.cpp
@@ -10,13 +10,13 @@ TEST_CASE("Testing CommandSender", "[Testing]") { // This test works
     CommandSender commandSender(io_service, "127.0.0.1", "5001");
 }
 
-TEST_CASE("Testing RobotController", "[Testing]") { // This test does not work
+TEST_CASE("Testing RobotController", "[Testing]") { // This test does not work, due to FrameReceiver failing to connect
     boost::asio::io_service io_service;
     CommandSender commandSender(io_service, "127.0.0.1", "5001");
     RobotController robotController("127.0.0.1", "5000", "127.0.0.1", "5001", io_service, commandSender);
 }
 
-TEST_CASE("Testing FrameReceiver", "[Testing]") { // This test does not work
+TEST_CASE("Testing FrameReceiver", "[Testing]") { // This test does not work, due to FrameReceiver failing to connect
     boost::asio::io_service io_service;
     FrameReceiver frameReceiver(io_service, "127.0.0.1", "5000");
 }
