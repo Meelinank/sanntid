0bda9ea39faf7e47c1ecf8bf9818af64be102ffb
Thomas Hellstrøm Olsen <74154916+Hellsparks@users.noreply.github.com>
2023-12-15 13:04:09 +0100

    introducing battery percentage

--- Python/App.py
+++ Python/App.py
@@ -158,8 +158,8 @@ class SpheroServer:
                 #self.rvr.sensor_control.add_sensor_data_handler(service=RvrStreamingServices.color_detection, handler=self.rvrColor_handler)
                 #self.rvr.sensor_control.add_sensor_data_handler(service=RvrStreamingServices.accelerometer, handler=self.rvrAccelhandler)
                 #self.rvr.sensor_control.add_sensor_data_handler(service=RvrStreamingServices.ambient_light, handler=self.rvrAmbientLight_handler)
-                #self.rvr.get_battery_percentage(handler=self.rvrBatteryPercentage_handler)
-                #print(f"Sending sensor data: {sensor_json}")
+                self.rvr.get_battery_percentage(handler=self.rvrBatteryPercentage_handler)
+                print(f"Sending sensor data: {sensor_json}")
                 client_socket.sendall(sensor_json.encode())
                 time.sleep(0.01)
             except Exception as e:
